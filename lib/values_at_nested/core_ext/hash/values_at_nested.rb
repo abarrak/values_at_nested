665ceaf0082acf0b62c4836541abbb9d9fb98d9e
Abdullah Barrak <a@abarrak.com>
2018-06-16 10:15:37 +0300

    implement values_at_nested by keys iterator

--- lib/values_at_nested/core_ext/hash/values_at_nested.rb
+++ lib/values_at_nested/core_ext/hash/values_at_nested.rb
@@ -3,12 +3,19 @@ class Hash
   # Returns an array that holds the values of the given keys list of various depth.
   #
   # === Example:
-  #   hash = { a: true, b: { x: true, y: false }, c: nil }
-  #   hash.values_at_nested(:b, :c)             # => { a: true }
-  #   hash.values_at_nested(:a, b: [:x])        # => { b: { y: false }, c: nil }
-  #   hash.values_at_nested(:a, b: [:x, :y])    # => { c: nil }
-  #   hash                                      # => { a: true, b: { x: true, y: false }, c: nil }
+  #   hash = { a: 58, b: { x: true, y: false }, c: nil }
+  #   hash.values_at_nested(:b, :c)             # => [58, nil]
+  #   hash.values_at_nested(:a, b: [:x])        # => [58, [true]]
+  #   hash.values_at_nested(:a, b: [:x, :y])    # => [58, [true, false]]
   #
   def values_at_nested(*nested_keys)
+    iterate_over_nested(nested_keys) { |k| self.dig(*k) }
+  end
+
+  def iterate_over_nested(keys)
+    keys.collect do |key|
+      key.is_a?(Hash) ? key.collect { |k, v| v.collect { |i| yield [k, i] } } : yield(key)
+    end.flatten
   end
 end
+
--- spec/values_at_nested_spec.rb
+++ spec/values_at_nested_spec.rb
@@ -5,4 +5,38 @@ RSpec.describe ValuesAtNested do
     expect(ValuesAtNested::VERSION).not_to be nil
     expect(ValuesAtNested::VERSION).to be_kind_of(String)
   end
+
+  describe "#values_at_nested hash extention" do
+    subject do
+      { name: 'Zohoor', degree: 'Psychology',
+        preferences: { color: 'mauve', drink: 'coffee', pet: 'cat' } }
+    end
+
+    it "accepts normal keys list like #values_at and filters accordingly" do
+    end
+
+    it "accepts nested keys list and filters accordingly" do
+    end
+
+    it "accepts keys list with single nested key and filters accordingly" do
+    end
+
+    it "returns empty array if no key arguments given" do
+    end
+
+    it "return all values if all keys are passed" do
+    end
+
+    context "3-level nested" do
+      subject { { a: 1, b: { b1: true, b2: false }, c: { d: { x: 100, y: 200 }, e: true } } }
+
+      it "accepts nested keys list in and filters accordingly" do
+        val = subject.values_at_nested(c: { d: :x })
+
+        expect(val).not_to eq(subject)
+        expect(val).not_to eq(subject.values)
+        expect(val).to eq(subject[:c][:d][:x])
+      end
+    end
+  end
 end
